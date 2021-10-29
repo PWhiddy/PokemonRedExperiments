62d30e1aa028552ef5d09d7ab8d0680ea5a8d4a9
PWhiddy <all.cows.like.to.moo@gmail.com>
2021-10-29 04:29:41 -0400

    add extra running info

--- run_game.py
+++ run_game.py
@@ -1,4 +1,5 @@
 
+from typing_extensions import Required
 from red_game import RedEnv
 from random_agent import RandomAgent
 
@@ -8,8 +9,12 @@ if __name__ == '__main__':
 
     count = 0
     total_reward = 0
+    max_reward = 0
+    min_reward = 999999
     while True:
         reward = sim.play_episode(rand_agent, 10000)
         count += 1
         total_reward += reward
-        print(f'round {count}, reward: {reward}, average: {total_reward/count}')
+        min_reward = min(min_reward, reward)
+        max_reward = max(max_reward, reward)
+        print(f'round {count}, reward: {reward}, min: {min_reward}, max: {max_reward}, average: {total_reward/count}')
