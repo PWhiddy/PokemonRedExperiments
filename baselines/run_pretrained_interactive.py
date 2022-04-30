8fb046d8978bf67e1868022ba10124e9623d2b45
Peter Whidden <peterwhidden@mac-studio.myfiosgateway.com>
2022-04-29 23:51:20 -0400

    interactive

---
+++ baselines/run_pretrained_interactive.py
@@ -0,0 +1,66 @@
+from os.path import exists
+from pathlib import Path
+import uuid
+from red_gym_env import RedGymEnv
+import gym
+from stable_baselines3 import A2C, PPO
+from stable_baselines3.common import env_checker
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+def make_env(rank, env_conf, seed=0):
+    """
+    Utility function for multiprocessed env.
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environments you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = RedGymEnv(env_conf)
+        env.seed(seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
+
+if __name__ == '__main__':
+
+    sess_path = Path(f'session_{str(uuid.uuid4())[:8]}')
+    ep_length = 2**24
+    env_config = {
+                'headless': False, 'save_final_state': True, 'early_stop': False,
+                'action_freq': 24, 'init_state': '../has_pokedex_nballs.state', 'max_steps': ep_length, 
+                'print_rewards': True, 'save_video': False, 'fast_video': True, 'session_path': sess_path,
+                'gb_path': '../PokemonRed.gb', 'debug': False, 'sim_frame_dist': 2_000_000.0
+            }
+    
+    num_cpu = 1 #64 #46  # Also sets the number of episodes per training iteration
+    env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
+    
+    #env_checker.check_env(env)
+    file_name = 'session_4da05e87_main_good/poke_439746560_steps'#'session_e41c9eff/poke_38207488_steps' #'session_e41c9eff/poke_250871808_steps'
+    
+    #'session_bfdca25a/poke_42532864_steps' #'session_d3033abb/poke_47579136_steps' #'session_a17cc1f5/poke_33546240_steps' #'session_e4bdca71/poke_8945664_steps' #'session_eb21989e/poke_40255488_steps' #'session_80f70ab4/poke_58982400_steps'
+    if exists(file_name + '.zip'):
+        print('\nloading checkpoint')
+        model = PPO.load(file_name, env=env, custom_objects={'lr_schedule': 0, 'clip_range': 0})
+        model.n_steps = ep_length
+        model.n_envs = num_cpu
+        model.rollout_buffer.buffer_size = ep_length
+        model.rollout_buffer.n_envs = num_cpu
+        model.rollout_buffer.reset()
+    else:
+        model = PPO('CnnPolicy', env, verbose=1, n_steps=ep_length, batch_size=512, n_epochs=1, gamma=0.999)
+    
+    agent_enabled = True
+    def toggle_agent():
+        agent_enabled = not agent_enabled
+    #keyboard.on_press_key("M", toggle_agent)
+    obs = env.reset()
+    while True:
+        action = 7 # pass action
+        if agent_enabled:
+            action, _states = model.predict(obs, deterministic=False)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
