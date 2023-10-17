78d02c28a9ee14962896a7c4bf18523862f3502e
Cassandra Ziemer <cassziemer@gmail.com>
2023-10-17 15:25:08 -0400

    init me

--- baselines/run_baseline_parallel.py
+++ baselines/run_baseline_parallel.py
@@ -40,13 +40,13 @@ if __name__ == '__main__':
     
     env_config = change_env(env_config, args)
     
-    num_cpu = 44 #64 #46  # Also sets the number of episodes per training iteration
+    num_cpu = 8 #64 #46  # Also sets the number of episodes per training iteration
     env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
     
     checkpoint_callback = CheckpointCallback(save_freq=ep_length, save_path=sess_path,
                                      name_prefix='poke')
     #env_checker.check_env(env)
-    learn_steps = 40
+    learn_steps = 1
     file_name = 'session_e41c9eff/poke_38207488_steps' #'session_e41c9eff/poke_250871808_steps'
     
     #'session_bfdca25a/poke_42532864_steps' #'session_d3033abb/poke_47579136_steps' #'session_a17cc1f5/poke_33546240_steps' #'session_e4bdca71/poke_8945664_steps' #'session_eb21989e/poke_40255488_steps' #'session_80f70ab4/poke_58982400_steps'
