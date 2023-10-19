428e97585afc14b59f59e62437a190701f3ba0d8
techmore <sdolbec1@gmail.com>
2023-10-18 23:49:06 -0400

    Updated with Tensorboard run_baseline_parallel.py

--- baselines/run_baseline_parallel.py
+++ baselines/run_baseline_parallel.py
@@ -12,6 +12,12 @@ import os
 import glob
 import re
 
+# TensorBoard log directory
+log_dir = "./tensorboard/"
+# Create log directory if it doesn't exist
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
 def make_env(rank, env_conf, seed=0):
     """
     Utility function for multiprocessed env.
@@ -43,13 +49,14 @@ def find_latest_session_and_poke():
                 most_recent_time = mod_time
                 most_recent_session = session_folder
                 most_recent_poke_file = poke_file[:-4]  # Remove '.zip' from the filename
-    return most_recent_session, most_recent_poke_file
 
+    return most_recent_session, most_recent_poke_file
 
 if __name__ == '__main__':
 
 
     ep_length = 2048 * 8
+    #ep_length = 2048 
     sess_path = f'session_{str(uuid.uuid4())[:8]}'
     args = get_args('run_baseline_parallel.py', ep_length=ep_length, sess_path=sess_path)
 
@@ -62,26 +69,36 @@ if __name__ == '__main__':
     
     env_config = change_env(env_config, args)
     
-    num_cpu = 44 #64 #46  # Also sets the number of episodes per training iteration
+    num_cpu = os.cpu_count()
+
+    #num_cpu = 44 #64 #46  # Also sets the number of episodes per training iteration
     env = SubprocVecEnv([make_env(i, env_config) for i in range(num_cpu)])
     
     checkpoint_callback = CheckpointCallback(save_freq=ep_length, save_path=sess_path,
                                      name_prefix='poke')
     #env_checker.check_env(env)
     learn_steps = 40
+    #file_name = 'session_e41c9eff/poke_38207488_steps' #'session_e41c9eff/poke_250871808_steps'
     session_folder, latest_poke_file = find_latest_session_and_poke()
-    #print('\n' + latest_poke_file)    
+    print('\n' + latest_poke_file)
     #'session_bfdca25a/poke_42532864_steps' #'session_d3033abb/poke_47579136_steps' #'session_a17cc1f5/poke_33546240_steps' #'session_e4bdca71/poke_8945664_steps' #'session_eb21989e/poke_40255488_steps' #'session_80f70ab4/poke_58982400_steps'
+    #if exists(file_name + '.zip'):
     if latest_poke_file:
         print('\nloading checkpoint')
-        model = PPO.load(latest_poke_file, env=env)
+        #model = PPO.load(latest_poke_file, env=env)
+        model = PPO.load(latest_poke_file, env=env, tensorboard_log=log_dir)
+
+        #print('\nloading checkpoint')
+        #model = PPO.load(file_name, env=env)
         model.n_steps = ep_length
         model.n_envs = num_cpu
         model.rollout_buffer.buffer_size = ep_length
         model.rollout_buffer.n_envs = num_cpu
         model.rollout_buffer.reset()
     else:
-        model = PPO('CnnPolicy', env, verbose=1, n_steps=ep_length, batch_size=512, n_epochs=1, gamma=0.999)
+        print('No valid session or poke file found.')
+        model = PPO('CnnPolicy', env, verbose=1, n_steps=ep_length, batch_size=512, n_epochs=1, gamma=0.999, tensorboard_log=log_dir)
+
     
     for i in range(learn_steps):
-        model.learn(total_timesteps=(ep_length)*num_cpu*1000, callback=checkpoint_callback)
+        model.learn(total_timesteps=(ep_length)*num_cpu*1000, callback=[checkpoint_callback])
