893fb2e53f0a5dd53d967bc99f1f8687c0f9697a
PWhiddy <all.cows.like.to.moo@gmail.com>
2024-10-10 02:51:27 -0400

    added leanke & thatguys full coordinate mapping

--- baselines/red_gym_env_v2.py
+++ baselines/red_gym_env_v2.py
@@ -13,6 +13,8 @@ from einops import repeat
 from gymnasium import Env, spaces
 from pyboy.utils import WindowEvent
 
+from global_map import local_to_global, GLOBAL_MAP_SHAPE
+
 event_flags_start = 0xD747
 event_flags_end = 0xD7F6 # 0xD761 # 0xD886 temporarily lower event flag range for obs input
 museum_ticket = (0xD754, 0)
@@ -128,8 +130,8 @@ class RedGymEnv(Env):
 
         self.agent_stats = []
 
-        self.explore_map_dim = 384
-        self.explore_map = np.zeros((self.explore_map_dim,self.explore_map_dim), dtype=np.uint8)
+        self.explore_map_dim = GLOBAL_MAP_SHAPE
+        self.explore_map = np.zeros(self.explore_map_dim, dtype=np.uint8)
 
         self.recent_screens = np.zeros( self.output_shape, dtype=np.uint8)
         
@@ -275,7 +277,6 @@ class RedGymEnv(Env):
                 "x": x_pos,
                 "y": y_pos,
                 "map": map_n,
-                "map_location": self.get_map_location(map_n),
                 "max_map_progress": self.max_map_progress,
                 "last_action": action,
                 "pcount": self.read_m(0xD163),
@@ -347,10 +348,7 @@ class RedGymEnv(Env):
 
     def get_global_coords(self):
         x_pos, y_pos, map_n = self.get_game_coords()
-        c = (np.array([x_pos,-y_pos])
-        + self.get_map_location(map_n)["coordinates"]
-        + self.coords_pad*2)
-        return self.explore_map.shape[0]-c[1], c[0]
+        return local_to_global(y_pos, x_pos, map_n)
 
     def update_explore_map(self):
         c = self.get_global_coords()
@@ -591,53 +589,3 @@ class RedGymEnv(Env):
             return self.essential_map_locations[map_idx]
         else:
             return -1
-
-    def get_map_location(self, map_idx):
-        map_locations = {
-            0: {"name": "Pallet Town", "coordinates": np.array([70, 7])},
-            1: {"name": "Viridian City", "coordinates": np.array([60, 79])},
-            2: {"name": "Pewter City", "coordinates": np.array([60, 187])},
-            3: {"name": "Cerulean City", "coordinates": np.array([240, 205])},
-            62: {"name": "Invaded house (Cerulean City)", "coordinates": np.array([290, 227])},
-            63: {"name": "trade house (Cerulean City)", "coordinates": np.array([290, 212])},
-            64: {"name": "Pokémon Center (Cerulean City)", "coordinates": np.array([290, 197])},
-            65: {"name": "Pokémon Gym (Cerulean City)", "coordinates": np.array([290, 182])},
-            66: {"name": "Bike Shop (Cerulean City)", "coordinates": np.array([290, 167])},
-            67: {"name": "Poké Mart (Cerulean City)", "coordinates": np.array([290, 152])},
-            35: {"name": "Route 24", "coordinates": np.array([250, 235])},
-            36: {"name": "Route 25", "coordinates": np.array([270, 267])},
-            12: {"name": "Route 1", "coordinates": np.array([70, 43])},
-            13: {"name": "Route 2", "coordinates": np.array([70, 151])},
-            14: {"name": "Route 3", "coordinates": np.array([100, 179])},
-            15: {"name": "Route 4", "coordinates": np.array([150, 197])},
-            33: {"name": "Route 22", "coordinates": np.array([20, 71])},
-            37: {"name": "Red house first", "coordinates": np.array([61, 9])},
-            38: {"name": "Red house second", "coordinates": np.array([61, 0])},
-            39: {"name": "Blues house", "coordinates": np.array([91, 9])},
-            40: {"name": "oaks lab", "coordinates": np.array([91, 1])},
-            41: {"name": "Pokémon Center (Viridian City)", "coordinates": np.array([100, 54])},
-            42: {"name": "Poké Mart (Viridian City)", "coordinates": np.array([100, 62])},
-            43: {"name": "School (Viridian City)", "coordinates": np.array([100, 79])},
-            44: {"name": "House 1 (Viridian City)", "coordinates": np.array([100, 71])},
-            47: {"name": "Gate (Viridian City/Pewter City) (Route 2)", "coordinates": np.array([91,143])},
-            49: {"name": "Gate (Route 2)", "coordinates": np.array([91,115])},
-            50: {"name": "Gate (Route 2/Viridian Forest) (Route 2)", "coordinates": np.array([91,115])},
-            51: {"name": "viridian forest", "coordinates": np.array([35, 144])},
-            52: {"name": "Pewter Museum (floor 1)", "coordinates": np.array([60, 196])},
-            53: {"name": "Pewter Museum (floor 2)", "coordinates": np.array([60, 205])},
-            54: {"name": "Pokémon Gym (Pewter City)", "coordinates": np.array([49, 176])},
-            55: {"name": "House with disobedient Nidoran♂ (Pewter City)", "coordinates": np.array([51, 184])},
-            56: {"name": "Poké Mart (Pewter City)", "coordinates": np.array([40, 170])},
-            57: {"name": "House with two Trainers (Pewter City)", "coordinates": np.array([51, 184])},
-            58: {"name": "Pokémon Center (Pewter City)", "coordinates": np.array([45, 161])},
-            59: {"name": "Mt. Moon (Route 3 entrance)", "coordinates": np.array([153, 234])},
-            60: {"name": "Mt. Moon Corridors", "coordinates": np.array([168, 253])},
-            61: {"name": "Mt. Moon Level 2", "coordinates": np.array([197, 253])},
-            68: {"name": "Pokémon Center (Route 3)", "coordinates": np.array([135, 197])},
-            193: {"name": "Badges check gate (Route 22)", "coordinates": np.array([0, 87])}, # TODO this coord is guessed, needs to be updated
-            230: {"name": "Badge Man House (Cerulean City)", "coordinates": np.array([290, 137])}
-        }
-        if map_idx in map_locations.keys():
-            return map_locations[map_idx]
-        else:
-            return {"name": "Unknown", "coordinates": np.array([80, 0])} # TODO once all maps are added this case won't be needed
